49b436d3391a8ac97d27a01e719aa0d6956afac5
William West <wwest466@gmail.com>
2020-08-19 13:57:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+
+arr2 = [];
+
+  array.each{|x| arr2 << badge_maker(x)}  end
+
+  return arr2
+
+end
